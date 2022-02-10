48cec2ecdea8aa9c85bbb47c1c347e3255baa612
Colin Prepscius <colinprepscius@gmail.com>
2022-02-09 21:23:47 -0500

    only double

--- src/ccd/config.h
+++ src/ccd/config.h
@@ -1,8 +1,8 @@
 #ifndef __CCD_CONFIG_H__
 #define __CCD_CONFIG_H__
 
-ifdef(`USE_SINGLE', `#define CCD_SINGLE')
-ifdef(`USE_DOUBLE', `#define CCD_DOUBLE')
+/* #define CCD_SINGLE */
+#define CCD_DOUBLE
 
 #endif /* __CCD_CONFIG_H__ */
   
